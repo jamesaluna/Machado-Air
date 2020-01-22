33cadb26972bb6f3367841b1d279abd41e61f04c
James L <jamesaluna1718@gmail.com>
2020-01-21 19:25:57 -0800

    updated 'App' component to showcase components from 'Components.js'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import Components from './Components';
+import {
+  BorderButton,
+  ContactButton,
+  Header
+} from './Components';
 import './App.css';
 
+import { ReactComponent as PhoneIcon } from './Vectors/phone.svg';
+
+const SvgVector = ({ source }) => <div>{source}</div>;
+
 function App() {
+  const componentDisplay = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(6, 1fr)',
+    gridTemplateRows: 'repeat(6, 1fr)',
+    width: '100vw',
+    height: '90vh',
+    marginTop: '200px',
+    padding: '50px'
+  }
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <div style={componentDisplay}>
+        <BorderButton name='Hello'/>
+        <ContactButton text='Button' iconName='phone'/>
+      </div>
     </div>
   );
 }
