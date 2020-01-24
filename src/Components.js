6ec448e32ce30887eee7f22f52670ddf51eaedca
James L <jamesaluna1718@gmail.com>
2020-01-23 18:48:00 -0800

    Added LargeImage, BorderDescription, and ImageDescription Components

--- src/Components.js
+++ src/Components.js
@@ -43,8 +43,8 @@ function ContactButton(props) {
 function LargeHeader() {
     return (
         <div className='DesktopHeader'>
-            <div><Logo/></div>
-            <div>
+            <div class='DesktopHeaderLogo'><Logo/></div>
+            <div class='DesktopHeaderLinks'>
                 <div>
                     <div><span>Home</span><span></span></div>
                     <div><span>About</span><span></span></div>
@@ -75,6 +75,113 @@ class Header extends React.Component {
     }
 }
 
+// Border Description (Contains header with pseudo element border and a description)
+// Contains title and content props for header and description 
+// Work in Progress... might need to add additional props for differing header lengths
+function BorderDescription(props) {
+    const header = props.title;
+    const content = props.content;
+    
+    if (props.direction == 'left') {
+        return (
+            <div className='borderDescription' direction='left'>
+                <h1>{props.title}</h1>
+                <p>{props.content}</p>
+            </div>
+        );
+    } else {
+        return (
+            <div className='borderDescription' direction='right'>
+                <h1>{props.title}</h1>
+                <p>{props.content}</p>
+            </div>
+        );
+    }
+}
+
+// Image Components (Will be used primarily for Landing Pages, my contain a background blob)
+// Add image source in 'img' attribtue, blobs are optional.
+
+function LargeImage(props) {
+    const image = props.img;
+
+    return (
+        <div>
+            <img className='largeImage' src={image} />
+        </div>
+    );
+}
+
+// Image and Description Component (Mainly for Landing Pages)
+// Direction is required ('ltr' == text left, image right --- 'rtl' == image left, text right)
+
+function ImageDescription(props) {
+    const title = props.title;
+    const description = props.description;
+    const image = props.image;
+    const direction = props.direction;
+
+    if (direction == 'ltr') {
+        return (
+            <div className='ImageDescription'>
+                <div position='left'>
+                    <div className='ImageDescriptionContent'>
+                        <BorderDescription title={title} content={description} direction='left'/>
+                    </div>
+                </div>
+                <div position='right'>
+                    <div className='ImageDescriptionContent'>
+                        <LargeImage img={image} />
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className='ImageDescription'>
+                <div position='left'>
+                    <div className='ImageDescriptionContent' direction='right'>
+                        <LargeImage img={image} />
+                    </div>
+                </div>
+                <div position='right'>
+                    <div className='ImageDescriptionContent'>
+                        <BorderDescription title={title} content={description} direction='right' />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// Component Wrapper (Used for blobs) 
+
+function ComponentWrapper(props) {
+    let comp_wrapper;
+    if (props.background && props.background_position) {
+        comp_wrapper = {
+            display: 'block',
+            width: '100vw',
+            height: '600px',
+            background: `url(${props.background})`,
+            backgroundSize: 'contain',
+            backgroundPosition: props.background_position,
+            backgroundRepeat: 'no-repeat'
+        }
+    } else {
+        comp_wrapper = {
+            display: 'block',
+            width: '100vw',
+            height: '600px',
+        }
+    }
+    return (
+        <div style={comp_wrapper}>
+            {props.children}
+        </div>
+    );
+}
+
 // Landing Page Component (For main pages)
 
 function MainPage(props) {
@@ -101,4 +208,4 @@ function MainPage(props) {
         </div>
     );
 }
-export {BorderButton, ContactButton, Header, MainPage};
+export {BorderButton, ContactButton, Header, MainPage, BorderDescription, LargeImage, ImageDescription, ComponentWrapper};
